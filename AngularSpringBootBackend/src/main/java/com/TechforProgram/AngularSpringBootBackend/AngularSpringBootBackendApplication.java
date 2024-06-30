66d88a06e4daa01d01acda890cb1645205402e47
jennykan <abc@email.com>
2024-06-30 15:38:24 +0800

    Build Angular Project using Maven

---
+++ AngularSpringBootBackend/src/main/java/com/TechforProgram/AngularSpringBootBackend/AngularSpringBootBackendApplication.java
@@ -0,0 +1,14 @@
+package com.TechforProgram.AngularSpringBootBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootBackendApplication.class, args);
+        System.out.println("backend running");
+	}
+
+}
---
+++ AngularSpringBootBackend/src/test/java/com/TechforProgram/AngularSpringBootBackend/AngularSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechforProgram.AngularSpringBootBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
